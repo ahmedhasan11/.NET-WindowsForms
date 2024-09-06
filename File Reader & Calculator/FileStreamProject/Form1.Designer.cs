f4fd93ac7589e4ebf7e77710a596b99a5d81d71f
Ahmed Hasan <121199845+ahmedhasan11@users.noreply.github.com>
2024-09-06 18:21:13 +0300

    Add files via upload

---
+++ File Reader & Calculator/FileStreamProject/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace FileStreamProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOperation = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRead1 = new System.Windows.Forms.Button();
+            this.btnRead2 = new System.Windows.Forms.Button();
+            this.btnReadOP = new System.Windows.Forms.Button();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtOperation
+            // 
+            this.txtOperation.Location = new System.Drawing.Point(130, 148);
+            this.txtOperation.Name = "txtOperation";
+            this.txtOperation.Size = new System.Drawing.Size(100, 22);
+            this.txtOperation.TabIndex = 0;
+            this.txtOperation.TextChanged += new System.EventHandler(this.txtOperation_TextChanged);
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(130, 78);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 22);
+            this.txtNum2.TabIndex = 1;
+            this.txtNum2.TextChanged += new System.EventHandler(this.txtNum2_TextChanged);
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(130, 18);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 22);
+            this.txtNum1.TabIndex = 2;
+            this.txtNum1.TextChanged += new System.EventHandler(this.txtNum1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "First Number :\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Second Number :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Operation :";
+            // 
+            // btnRead1
+            // 
+            this.btnRead1.Location = new System.Drawing.Point(281, 18);
+            this.btnRead1.Name = "btnRead1";
+            this.btnRead1.Size = new System.Drawing.Size(127, 23);
+            this.btnRead1.TabIndex = 6;
+            this.btnRead1.Text = "Get Num1";
+            this.btnRead1.UseVisualStyleBackColor = true;
+            this.btnRead1.Click += new System.EventHandler(this.btnRead1_Click);
+            // 
+            // btnRead2
+            // 
+            this.btnRead2.Location = new System.Drawing.Point(281, 78);
+            this.btnRead2.Name = "btnRead2";
+            this.btnRead2.Size = new System.Drawing.Size(127, 23);
+            this.btnRead2.TabIndex = 7;
+            this.btnRead2.Text = "Get Num2";
+            this.btnRead2.UseVisualStyleBackColor = true;
+            this.btnRead2.Click += new System.EventHandler(this.btnRead2_Click);
+            // 
+            // btnReadOP
+            // 
+            this.btnReadOP.Location = new System.Drawing.Point(281, 151);
+            this.btnReadOP.Name = "btnReadOP";
+            this.btnReadOP.Size = new System.Drawing.Size(127, 23);
+            this.btnReadOP.TabIndex = 8;
+            this.btnReadOP.Text = "Get OP";
+            this.btnReadOP.UseVisualStyleBackColor = true;
+            this.btnReadOP.Click += new System.EventHandler(this.btnReadOP_Click);
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(281, 211);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(127, 23);
+            this.btnResult.TabIndex = 9;
+            this.btnResult.Text = "Show Result";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(130, 212);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 22);
+            this.txtResult.TabIndex = 10;
+            this.txtResult.TextChanged += new System.EventHandler(this.txtResult_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 218);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Result :";
+            // 
+            // txtExit
+            // 
+            this.txtExit.Location = new System.Drawing.Point(167, 272);
+            this.txtExit.Name = "txtExit";
+            this.txtExit.Size = new System.Drawing.Size(174, 23);
+            this.txtExit.TabIndex = 12;
+            this.txtExit.Text = "Exit";
+            this.txtExit.UseVisualStyleBackColor = true;
+            this.txtExit.Click += new System.EventHandler(this.txtExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtExit);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.btnReadOP);
+            this.Controls.Add(this.btnRead2);
+            this.Controls.Add(this.btnRead1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtOperation);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOperation;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRead1;
+        private System.Windows.Forms.Button btnRead2;
+        private System.Windows.Forms.Button btnReadOP;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button txtExit;
+    }
+}
+
---
+++ File Reader & Calculator/FileStreamProject/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Web;
+
+namespace FileStreamProject
+{
+
+    public partial class Form1 : Form
+    {
+        FileStream rs = new FileStream("num1.txt", FileMode.Open, FileAccess.Read);
+
+        FileStream fs = new FileStream("num2.txt", FileMode.Open, FileAccess.Read);
+
+        FileStream ss = new FileStream("operator.txt", FileMode.Open, FileAccess.Read);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNum1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRead1_Click(object sender, EventArgs e)
+        {
+            StreamReader num1 = new StreamReader(rs);
+             int s = int.Parse(num1.ReadLine());
+            txtNum1.Text = s.ToString();
+        }
+
+        private void txtNum2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRead2_Click(object sender, EventArgs e)
+        {
+            StreamReader num2 = new StreamReader(fs);
+            int m = int.Parse(num2.ReadLine());
+            txtNum2.Text = m.ToString();
+        }
+
+        private void txtOperation_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReadOP_Click(object sender, EventArgs e)
+        {
+            StreamReader OP = new StreamReader(ss);
+            string n =OP.ReadLine();
+            txtOperation.Text = n.ToString();
+        }
+
+        private void txtResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            int x;
+             switch (txtOperation.Text) {
+                case "+" :
+                    x = int.Parse(txtNum1.Text) + int.Parse(txtNum2.Text);
+                    txtResult.Text = x.ToString();
+                    break;
+                case "-" :
+                    x = int.Parse(txtNum1.Text) - int.Parse(txtNum2.Text);
+                    txtResult.Text = x.ToString();
+                    break;
+                case "*":
+                    x = int.Parse(txtNum1.Text) * int.Parse(txtNum2.Text);
+                    txtResult.Text = x.ToString();
+                    break;
+                    case "/":
+                    x = int.Parse(txtNum1.Text) / int.Parse(txtNum2.Text);
+                    txtResult.Text = x.ToString();
+                    break;
+                case "%":
+                    x = int.Parse(txtNum1.Text) % int.Parse(txtNum2.Text);
+                    txtResult.Text = x.ToString();
+                    break;
+            } 
+        }
+
+        private void txtExit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("GoodBye User");
+        }
+    }
+}
